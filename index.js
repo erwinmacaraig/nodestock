1c4b521b7dd321a572f092f0d07a5093b7350cea
Erwin Macaraig <erwin.macaraig@gmail.com>
2021-09-03 11:33:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 5000;
+
+// Set handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set handlebars routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff: 'This is stuff'
+    });
+});
+
+
+// set a static folder and point our app
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => { console.log(`SERVER LISTENING ON PORT ${PORT}`)});
